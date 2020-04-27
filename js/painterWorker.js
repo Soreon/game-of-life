5a1d63c93f83725f5578814d6cf810224ef0c3eb
Soreon <soreon@hotmail.fr>
2020-04-27 20:56:14 +0200

    Ajout de workers

---
+++ js/painterWorker.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-bitwise */
+/* eslint-disable no-param-reassign */
+
+let canvas = null;
+let context = null;
+let r = null;
+let grid = [];
+const colorGradient = [
+  '#000000',
+  '#401000',
+  '#862100',
+  '#ad2b00',
+  '#FF0000',
+  '#FF5500',
+  '#FF8000',
+  '#FFB300',
+  '#FFDD00',
+  '#FFFFFF',
+];
+
+function setupCanvas() {
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#FFFFFF';
+  ctx.imageSmoothingEnabled = false;
+  return ctx;
+}
+
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function rectangle(x, y, width, height, color) {
+  context.fillStyle = color;
+  context.fillRect(x, y, width, height);
+}
+
+function drawGrid() {
+  for (let t = 0; t < r * r; t += 1) {
+    if (grid[t] > 0) rectangle((t % r), ~~(t / r), 1, 1, colorGradient[grid[t]]);
+  }
+}
+
+function animate() {
+  clearCanvas();
+  drawGrid(grid);
+  requestAnimationFrame(animate);
+}
+
+
+onmessage = (e) => {
+  if (e.data.type === 'init') {
+    ({ canvas, r } = e.data);
+    context = setupCanvas();
+    animate();
+  } else if (e.data.type === 'mess') {
+    ({ grid } = e.data);
+  }
+};
--- js/script.js
+++ js/script.js
@@ -1,80 +1,29 @@
 /* eslint-disable no-bitwise, no-param-reassign */
 
+const computerWorker = new Worker('js/computerWorker.js');
+const painterWorker = new Worker('js/painterWorker.js');
+
 const canvas = document.getElementById('main-canvas');
-const context = canvas.getContext('2d');
 const r = 200;
-const cellW = canvas.clientWidth / r;
-const cellH = canvas.clientHeight / r;
-let grid1 = [];
-let grid2 = [];
+canvas.height = r;
+canvas.width = r;
+const offscreen = canvas.transferControlToOffscreen();
+
+
+let grid1 = new Uint8Array(r ** 2);
 
 function initGrid(grid) {
-  for (let t = 0; t < r * r; t += 1) {
-    grid[t] = ~~(Math.random() * 2);
+  for (let t = 0; t < r ** 2; t += 1) {
+    grid[t] = Math.random() * 3 > 2 ? 9 : 0;
   }
 }
 
-function initGame() {
-  initGrid(grid1);
-}
+computerWorker.onmessage = (e) => {
+  grid1 = e.data;
+  painterWorker.postMessage({ type: 'mess', grid: grid1 });
+  computerWorker.postMessage([grid1, r]);
+};
 
-function rectangle(x, y, width, height, color) {
-  context.beginPath();
-  context.rect(x, y, width, height);
-  context.fillStyle = color;
-  context.fill();
-  context.closePath();
-}
-
-function clearCanvas() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function drawGrid(grid) {
-  for (let t = 0; t < r * r; t += 1) {
-    if (grid[t] === 1) rectangle((t % r) * cellW, ~~(t / r) * cellH, cellW, cellH, '#FFFFFF');
-  }
-}
-
-function getNeighborSum(grid, t) {
-  let sum = 0;
-  sum += grid[t - r - 1] || 0;
-  sum += grid[t - r] || 0;
-  sum += grid[(t - r) + 1] || 0;
-  sum += grid[t - 1] || 0;
-  sum += grid[t + 1] || 0;
-  sum += grid[(t + r) - 1] || 0;
-  sum += grid[t + r] || 0;
-  sum += grid[t + r + 1] || 0;
-
-  return sum;
-}
-
-function computeNextGrid(grid) {
-  grid2 = [];
-  for (let t = 0; t < r * r; t += 1) {
-    const ns = getNeighborSum(grid, t, r);
-    if (ns === 3) {
-      grid2[t] = 1;
-    } else if (ns < 2 || ns > 3) {
-      grid2[t] = 0;
-    } else if (ns === 2) {
-      grid2[t] = grid[t];
-    }
-  }
-  return grid2;
-}
-
-function draw() {
-  clearCanvas();
-  drawGrid(grid1);
-}
-
-function animate() {
-  draw();
-  grid1 = computeNextGrid(grid1);
-  requestAnimationFrame(animate);
-}
-
-initGame();
-animate();
+initGrid(grid1);
+painterWorker.postMessage({ type: 'init', canvas: offscreen, r }, [offscreen]);
+computerWorker.postMessage([grid1, r]);
